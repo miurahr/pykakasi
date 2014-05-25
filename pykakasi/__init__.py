3d828a5ceb02aa2cba87b809f69c81734100c4af
Hiroshi Miura <miurahr@linux.com>
2014-05-25 14:33:10 +0900

    fix  no found J2a
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- pykakasi/__init__.py
+++ pykakasi/__init__.py
@@ -5,6 +5,7 @@ from .k2a import K2a
 from .h2a import H2a
 from .h2k import H2K
 from .k2h import K2H
+from .j2a import J2a
 from .symbols import sym2
 from .kakasi import kakasi
 from .wakati import wakati
