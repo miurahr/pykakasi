e15fc1095e275bd262f80b4f3e38e95f36291116
Hiroshi Miura <miurahr@linux.com>
2015-06-07 18:07:59 +0900

    add missing itaiji.py
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ pykakasi/itaiji.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# itaiji.py
+#
+# Copyright 2015 Hiroshi Miura <miurahr@linux.com>
+#
+import re
+from pkg_resources import resource_filename
+
+try: # pragma: no cover
+    import cPickle as pickle
+except: # pragma: no cover
+    import pickle
+
+class itaiji (object):
+
+    _itaijidict = None
+    _itaijidict_len = 0
+
+ # this class is Borg/Singleton
+    _shared_state = {}
+
+    def __new__(cls, *p, **k):
+        self = object.__new__(cls, *p, **k)
+        self.__dict__ = cls._shared_state
+        return self
+
+    def __init__(self):
+        if self._itaijidict is None:
+            itaijipath = resource_filename(__name__, 'itaijidict2.pickle')
+            itaiji_pkl = open(itaijipath, 'rb')
+            (self._itaijidict, self._itaijidict_len) = pickle.load(itaiji_pkl)
+
+    def haskey(self, key):
+        return key in self._itaijidict
+
+    def lookup(self, key):
+        return self._itaijidict[key]
+
+    def canConvert(self, c):
+        return ( 0x3400 <= ord(c[0]) and ord(c[0]) < 0xfa2e)
+
+    def convert(self, text):
+        r = []
+        for c in text:
+            if self.haskey(c):
+                r.append(c)
+        for c in r:
+            text = re.sub(c, self.lookup(c), text)
+        return text
