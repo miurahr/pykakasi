332460ec5605e2e0b7ce007ca1561183ea94a2de
Hiroshi Miura <miurahr@linux.com>
2015-06-07 11:34:54 +0900

    reorganize tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,3 @@
-from sys import version_info
-
-if version_info[0] >= 3:
-    from .basic_3 import *
-    from .advanced_3 import *
-else:
-    from .basic_2 import *
-    from .advanced_2 import *
-
-from .genkanwadict import *
+from .test_pykakasi import *
+from .test_pykakasi_advanced import *
+from .test_genkanwadict import *
