21968f3aa334a0e62569cc8d6f97079554f94cf5
Hiroshi Miura <miurahr@linux.com>
2018-03-27 13:16:16 +0900

    Move to semidbm from anydbm
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- pykakasi/kanwa.py
+++ pykakasi/kanwa.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 #  kanwa.py
 #
-# Copyright 2011-2015 Hiroshi Miura <miurahr@linux.com>
+# Copyright 2011-2018 Hiroshi Miura <miurahr@linux.com>
 from zlib import decompress
 from pkg_resources import resource_filename
 from marshal import loads
 import six
-
-try:
-    import anydbm as dbm
-except:
-    import dbm
+import semidbm as dbm
 
 class kanwa (object):
+
+# This class is Borg/Singleton
+# It provides same results becase lookup from a static dictionary.
+# There is no state rather dictionary dbm.
+    _shared_state = {}
 
     _kanwadict = None
     _jisyo_table = {}
 
-# this class is Borg/Singleton
-    _shared_state = {}
+# FIXME: there is no invalidate mechanism for _jisyo_table data.
+#        It can lead a large memory consumption in long live process.
+#        but max size are limited to a size of a dictionary.
 
     def __new__(cls, *p, **k):
         self = object.__new__(cls, *p, **k)
@@ -27,7 +29,7 @@ class kanwa (object):
 
     def __init__(self):
         if self._kanwadict is None:
-            dictpath = resource_filename(__name__, 'kanwadict2.db')
+            dictpath = resource_filename(__name__, 'kanwadict3.db') # FIXME: no hardcoded filename
             self._kanwadict = dbm.open(dictpath,'r')
 
     def load(self, char):
