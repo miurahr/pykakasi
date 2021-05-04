a11253c242794cee3924a40cc1ea4bef2c703e39
Hiroshi Miura <miurahr@linux.com>
2021-05-04 13:05:19 +0900

    Add profiling test
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ utils/prof_result.py
@@ -0,0 +1,22 @@
+import argparse
+import pathlib
+import pstats
+import sys
+
+
+def print_prof(file):
+    p = pstats.Stats(str(file))
+    p.strip_dirs()
+    p.sort_stats(pstats.SortKey.CUMULATIVE)
+    p.print_stats(r"(kanji|kakasi|__init__|exceptions|properties|scripts).py:")
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='prof_result')
+    parser.add_argument('prof', type=pathlib.Path, help='pytest-profile saved prof.')
+    args = parser.parse_args()
+    print_prof(args.prof)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
