8df36b8d935fb46e2dfa499b11b6248ce2f49220
Hiroshi Miura <miurahr@linux.com>
2021-05-18 08:46:34 +0900

    test: add from unihandecode
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_pykakasi_structured.py
+++ tests/test_pykakasi_structured.py
@@ -515,6 +515,27 @@ def test_kakasi_unidic_noun(case, expected):
                 },
             ],
         ),
+        (
+            "\uF862\u6709\u9650\u4F1A\u793E",  # Adobe CID 8321
+            [
+                {
+                    "orig": "\uF862",
+                    "hira": "",
+                    "kana": "",
+                    "hepburn": "",
+                    "kunrei": "",
+                    "passport": "",
+                },
+                {
+                    "orig": "\u6709\u9650\u4F1A\u793E",
+                    "hira": "ゆうげんがいしゃ",
+                    "kana": "ユウゲンガイシャ",
+                    "hepburn": "yuugengaisha",
+                    "kunrei": "yuugengaisya",
+                    "passport": "yuugengaisha"
+                }
+            ]
+        )
     ],
 )
 def test_kakasi_unihandecode(case, expected):
