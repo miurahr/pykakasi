f5b6d5d6b54c50e5384558292675f8ccd2d05b3b
Hiroshi Miura <miurahr@linux.com>
2024-06-23 16:37:57 +0900

    fix: pytest config for test db

--- src/pykakasi/properties.py
+++ src/pykakasi/properties.py
@@ -3,14 +3,17 @@
 #
 # Copyright 2019 Hiroshi Miura <miurahr@linux.com>
 
-from contextlib import ExitStack
-from importlib_resources import as_file, files
 import os
+from pathlib import Path
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import importlib_resources as pkg_resources
 
 
 class Configurations:
 
-    data_path = ExitStack().enter_context(as_file(files(__name__) / "data"))
+    data_path = pkg_resources.files(__package__) / "data"
     jisyo_hepburn_hira = "hepburnhira3.db"
     jisyo_passport_hira = "passporthira3.db"
     jisyo_kunrei_hira = "kunreihira3.db"
